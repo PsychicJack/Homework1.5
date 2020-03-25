7d096008679636872b1dca71e5d2640200acf190
PsychicJack <supremeruler98@gmail.com>
2020-03-25 20:55:46 +0100

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import { getMovieNames, getMovie, getActors } from "./movie.service";
 import { drawInit } from "./draw-functions";
 
-console.log("hello again");
 
 getMovieNames().then(movies => {
     drawInit(document.body, movies);
 });
-
-
-getActors(1).then(result => console.log(result));
